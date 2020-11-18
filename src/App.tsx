2842b68fde51387c048bbab7e95265449aa9ba42
Aaron Powell <me@aaron-powell.com>
2020-11-18 23:30:58 +0000

    making changes

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 😁
+          Learn React 🤩
         </a>
         {message && <p>{message}</p>}
       </header>
